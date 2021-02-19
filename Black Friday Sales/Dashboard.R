6e1667f1b3d6dbab3830a6dc791ea56d8d3a00fe
Sachin Shubham <34959756+sachinshubhams@users.noreply.github.com>
2021-02-19 00:49:38 -0600

    Add files via upload

---
+++ Black Friday Sales/Dashboard.R
@@ -0,0 +1,46 @@
+ui<-shinyUI(
+  dashboardPage(
+    dashboardHeader(title = "BLACK FRIDAY SALES",titleWidth = 300,
+                    dropdownMenu(type = "messages",
+                                 messageItem(
+                                   from = "Sachin Shubham",
+                                   message = "Check my Github here",
+                                   href = ''
+                                 ),
+                                 messageItem(
+                                   from = "Black friday sales Datahack",
+                                   message = "How do I register?",
+                                   icon = icon("question"),
+                                   href = ''
+                                 ))),
+    dashboardSidebar(
+      sidebarMenu(id = 'sidebarmenu',
+                  # first menu item
+                  menuItem("Summary",tabName = "summary", icon = icon("dashboard")),
+                  menuItem("Demographics Insights",tabName = "insight", icon = icon("dashboard")),
+                  menuItem("Product Insights",tabName = "insight_2", icon = icon("dashboard")),
+                  menuItem("Univariate Plots",tabName = "uni", icon = icon("bar-chart")),
+                  # second menu item with 2 sub menus
+                  menuItem("Bivariate Plots",
+                           icon = icon('bar-chart'),
+                           tabName = 'chart1'
+                           
+                  ))),
+    
+    
+    dashboardBody(
+      tabItems(
+        tabItem("summary"),
+        tabItem("insight"),
+        tabItem("insight_2"),
+        tabItem("uni"),
+        tabItem("chart1")
+        
+      )
+    ),skin = 'red'
+  )
+)
+
+server <- function(input, output,session){}
+
+shinyApp(ui, server)
